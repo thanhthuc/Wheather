011fa3b55f648032391a62c2db7e3d4c5ca06a4e
Nguyen Thanh Thuc <nguyenthanhthuc@Nguyens-MacBook-Pro.local>
2021-10-09 08:41:15 +0700

    Init project

---
+++ Wheather/Wheather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheather
+//
+//  Created by Nguyen Thanh Thuc on 09/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
